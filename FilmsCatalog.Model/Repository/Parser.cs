37230a5ee6a4976e76fd827ffc073b8112bb6b60
horbel <gorbel.alex@ya.ru>
2016-02-06 08:18:36 +0300

    push unversioned files

---
+++ FilmsCatalog.Model/Repository/Parser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace FilmsCatalog.Model.Repository
+{
+    public class Parser
+    {
+        public static string GetText()
+        {
+            var webGet = new HtmlWeb();
+            var doc = webGet.Load("http://kinogo.co/1839-odin-doma-1990.html");
+           
+            var ourNode = doc.DocumentNode.SelectSingleNode("//img[@title='Один дома (1990)']");
+            if (ourNode != null)
+                return ourNode.InnerHtml;
+            else
+                return "nothing found";
+        }
+
+
+        public static void Main()
+        {
+           Console.WriteLine(GetText());
+            
+
+        }
+    }
+
+}
