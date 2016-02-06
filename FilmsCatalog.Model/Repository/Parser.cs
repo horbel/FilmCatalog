027d306eb214122f40a10a19a058877dbfe97286
horbel <gorbel.alex@ya.ru>
2016-02-06 15:21:15 +0300

    added a new function in Parser

--- FilmsCatalog.Model/Repository/Parser.cs
+++ FilmsCatalog.Model/Repository/Parser.cs
@@ -6,18 +6,24 @@ using System.Web;
 using System.IO;
 using System.Net;
 using System.Threading.Tasks;
+using System.Diagnostics;
 using HtmlAgilityPack;
 
 namespace FilmsCatalog.Model.Repository
 {
     public class Parser
     {
-        public static string GetText()
+        public static string GetText1()
         {
-            var webGet = new HtmlWeb();
+            var webGet = new HtmlWeb()
+            {
+                AutoDetectEncoding = false,
+                OverrideEncoding = Encoding.UTF8,
+            };
             var doc = webGet.Load("http://kinogo.co/1839-odin-doma-1990.html");
            
-            var ourNode = doc.DocumentNode.SelectSingleNode("//img[@title='Один дома (1990)']");
+            var ourNode = doc.DocumentNode.SelectSingleNode("//div[@id='news-id-1839']");
+
             if (ourNode != null)
                 return ourNode.InnerHtml;
             else
@@ -25,10 +31,43 @@ namespace FilmsCatalog.Model.Repository
         }
 
 
+
+        public static void  GetText2()
+        {
+            List<string> outList = new List<string>();
+
+            string html = "https://yandex.by/search/?numdoc=10&p=0&rdrnd=601861&text=kinogo.co%20Один%20дома%201990%20&lr=157";
+            HtmlDocument HD = new HtmlDocument();
+            var web = new HtmlWeb
+            {
+                AutoDetectEncoding = false,
+                OverrideEncoding = Encoding.UTF8 //GetEncoding("windows-1251")
+            };
+
+            HD = web.Load(html);
+
+            
+            HtmlNodeCollection NoAltElements = HD.DocumentNode.SelectNodes("//div");
+            
+            ///допилить
+            if (NoAltElements != null)
+            {
+                foreach(HtmlNode node in NoAltElements)
+                {
+
+                    string outputText = node.InnerHtml;
+                    Console.WriteLine(outputText);
+                }
+            }
+            else
+                Console.WriteLine("found nothing");
+        }
+        //шаблон запроса яндекс http://yandex.ru/yandsearch?text=[KEYWORD]&p=[PAGE_NUMBER]&numdoc=[RESULTS_ON_PAGE]
         public static void Main()
         {
-           Console.WriteLine(GetText());
-            
+            //Console.WriteLine(GetText1());
+            GetText2();
+            //Process.Start("http://yandex.ru/yandsearch?text=kinogo.co Один дома 1990 &p=0&numdoc=10");
 
         }
     }
